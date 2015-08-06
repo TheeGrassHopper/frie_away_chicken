beba0e6dbf934b4d0c47a3ccf7a1746d45ca859a
TheeGrassHopper <almortada88@gmail.com>
2015-08-05 21:27:04 -0700

    AWS Endpoint Error When Uploading Files Using Paperclip Deploying to Heroku

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -14,4 +14,14 @@ class Recipe < ActiveRecord::Base
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  def s3_credentials
+                    {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region => 'us-east-1',
+    :s3_host_name => 'frie-away-chicken.s3-website-us-east-1.amazonaws.com',
+    :url => ':s3_domain_url'
+  }
+  end
 end
