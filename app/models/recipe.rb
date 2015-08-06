d9a0688e404e0a7140c280019704d5167bf92cd4
TheeGrassHopper <almortada88@gmail.com>
2015-08-05 21:24:48 -0700

    AWS Endpoint Error When Uploading Files Using Paperclip Deploying to Heroku

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,7 +10,7 @@ class Recipe < ActiveRecord::Base
  		reject_if: proc { |attributes| attributes['step'].blank? },
   			allow_destroy: true
  	validates :title, :description, :image, presence: true
-	has_attached_file :image, styles: { :medium => "400x400#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",:download,
+	has_attached_file :image, styles: { :medium => "400x400#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",:image,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
