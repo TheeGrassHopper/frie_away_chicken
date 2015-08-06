24cd4f0fd8c157477b53e72a8bf39f1606e21a79
TheeGrassHopper <almortada88@gmail.com>
2015-08-05 21:02:55 -0700

    AWS Endpoint Error When Uploading Files Using Paperclip Deploying to Heroku

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,4 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options.merge!(
-:storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => 'us-west-2',
-    :s3_host_name => 's3-us-west-2.amazonaws.com',
-    :url => ':s3_domain_url'
-  }
-)
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
