bd050dd4ce9116c8b50655f802999a034098f502
TheeGrassHopper <almortada88@gmail.com>
2015-08-05 21:33:49 -0700

    AWS Endpoint Error When Uploading Files Using Paperclip Deploying to Heroku

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,16 +10,6 @@ class Recipe < ActiveRecord::Base
  		reject_if: proc { |attributes| attributes['step'].blank? },
   			allow_destroy: true
  	validates :title, :description, :image, presence: true
-	has_attached_file :image, styles: { :medium => "400x400#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",:image,
-                    :storage => :s3,
-                    :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => 'us-east-1',
-    :s3_host_name => 'frie-away-chicken.s3-website-us-east-1.amazonaws.com',
-    :url => ':s3_domain_url'
-  }
+	has_attached_file :image, styles: { :medium => "400x400#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  d
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 'frie-away-chicken.s3-website-us-east-1.amazonaws.com'
 Paperclip::Attachment.default_options[:default_url] = '/images/default_image.png'
