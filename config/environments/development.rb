db10810e00db1cd836c11654c01cf06255bf4b0f
TheeGrassHopper <almortada88@gmail.com>
2015-08-06 17:28:14 -0700

    AWS Endpoint Error When Uploading Files Using Paperclip Deploying to Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,14 +40,14 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
-   :bucket => ENV['S3_BUCKET_NAME'],
+
   :s3_credentials => {
-   
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :region => 'us-east-1',
-    :s3_host_name => 'frie-away-chicken.s3-website-us-east-1.amazonaws.com',
-    :url => ':s3_domain_url'
+    # :s3_host_name => 'frie-away-chicken.s3-website-us-east-1.amazonaws.com',
+    # :url => ':s3_domain_url'
   }
 }
   # Raises error for missing translations
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   # Sets Paperclip to upload images tp Amazon S3
   config.paperclip_defaults = {
   :storage => :s3,
-  :bucket => ENV['S3_BUCKET_NAME'],
+  
   :s3_credentials => {
-    
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :region => 'us-east-1',
